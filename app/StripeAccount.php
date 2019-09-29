e3a03799b686130c08a02dbbeea3f0c5c18b32bd
Saqib <isaqib23@gmail.com>
2019-09-29 20:15:22 +0500

    Stripe Standard Account

--- app/StripeAccount.php
+++ app/StripeAccount.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StripeAccount extends Model
 {
-    protected $table = 'subscriptions';
+    protected $table = 'stripe_accounts';
     protected $fillable = [
         'user_id', 'stripe_publishable_key', 'stripe_user_id', 'refresh_token', 'access_token'
     ];
