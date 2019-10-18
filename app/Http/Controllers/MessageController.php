1573660b0a5a00ecf6e43ac88111e67833d1879f
Saqib <isaqib23@gmail.com>
2019-10-18 18:47:43 +0500

    test

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -132,7 +132,7 @@ class MessageController extends Controller
 
             $response = $messages->with(['user','group'])
                 ->where(['user_id' => $request->input('user_id'), 'group_id' => $request->input('group_id')])
-                ->orderBy('id','desc')->paginate(2,['*'],'page',$request->input('page'));
+                ->orderBy('id','desc')->get();
             return response()->json([
                 'status'    =>  true,
                 'message'   => 'Messages List Fetched Successfully!',
